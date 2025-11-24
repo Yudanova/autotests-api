5b99389c2ef099f78f9f8e6ccf97d55414a0410b
Yudanova <a2798252y@gmail.com>
2025-11-24 10:54:55 +0200

    Registering markers for test

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,4 +1,5 @@
 from http import HTTPStatus
+import pytest
 
 from clients.users.public_users_client import get_public_users_client
 from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
@@ -9,7 +10,8 @@ from tools.assertions.schema import validate_json_schema
 # Import the assertion function for validating user creation response
 from tools.assertions.users import assert_create_user_response
 
-
+@pytest.mark.users
+@pytest.mark.regression
 def test_create_user():
     public_users_client = get_public_users_client()
 
@@ -29,3 +31,5 @@ def test_create_user():
     # assert response_data.user.middle_name == request.middle_name, 'Incorrect user middle_name'
 
     validate_json_schema(response.json(), response_data.model_json_schema())
+
+    # python -m pytest -m users -s -v
